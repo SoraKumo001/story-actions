c498f65772884e694a8ebc9f4fb4dfeaabdeba40
info@croud.jp <info@croud.jp>
2020-07-05 14:39:14 +0900

    サンプル追加

--- pages/index.stories.tsx
+++ pages/index.stories.tsx
@@ -3,5 +3,5 @@ import { storiesOf } from "@storybook/react";
 import { Box } from "./Box";
 
 storiesOf("Component", module)
-  .add("Box1", () => <Box>あああああ</Box>)
-  .add("Box2", () => <Box>いいいい</Box>);
+  .add("Box1", () => <Box>サンプル</Box>)
+  .add("Box2", () => <Box>追加</Box>);
